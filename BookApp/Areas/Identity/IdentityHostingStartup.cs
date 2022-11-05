52443ad84d4ea0b112c880d26ce1fcee63361cd5
emramadhan <87051168+emramadhan@users.noreply.github.com>
2022-11-05 17:38:11 +0700

    Add files via upload

---
+++ BookApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BookApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BookApp.Areas.Identity.IdentityHostingStartup))]
+namespace BookApp.Areas.Identity
+{
+   public class IdentityHostingStartup : IHostingStartup
+   {
+      public void Configure(IWebHostBuilder builder)
+      {
+         builder.ConfigureServices((context, services) =>
+         {
+            services.AddDbContext<BookAppContext>(options =>
+                options.UseSqlServer(
+                    context.Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+               .AddRoles<IdentityRole>()
+               .AddEntityFrameworkStores<BookAppContext>();
+         });
+      }
+   }
+}
